5ddf3a61f1968f9bc094fb8bdc0f8b725ce21d5a
nouman <noumanharry7@gmail.com>
2022-02-19 05:21:26 +0500

    lastr changes

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = document.getElementById('name');
+var lastname = document.getElementById('lname');
+var email = document.getElementById('email')
+
+document.getElementById('submit').addEventListener('onclick', function (e) {
+  e.preventDefault;
+
+  validationall();
+  
+})
+
